eb7a378f8be1ef1b5512d3160729b820028c831a
Amer, Qsem <amerm4011@gmail.com>
2022-01-14 02:00:53 +0800

    my portfolio v-0.1

--- script.js
+++ script.js
@@ -4,3 +4,44 @@ let body = document.querySelector("body");
 menuToggler.onclick = function () {
     body.classList.toggle("open");
 };
+
+// Change navbar color on scroll
+let myNav = document.getElementById("myNav");
+window.onscroll = function () {
+    if (window.scrollY > 680) {
+        myNav.style.backgroundColor = "#252525";
+        myNav.style.boxShadow = "0 0 10px #252525";
+    } else {
+        myNav.style.backgroundColor = "transparent";
+        myNav.style.boxShadow = "none";
+    };
+};
+
+// Scroll Animation
+window.sa = ScrollReveal();
+sa.reveal(".animate-left", {
+    origin: "left",
+    duration: 1000,
+    distance: "400px",
+    delay: 300
+});
+sa.reveal(".animate-right", {
+    origin: "right",
+    duration: 1000,
+    distance: "400px",
+    delay: 600
+});
+sa.reveal(".animate-top", {
+    origin: "top",
+    duration: 1000,
+    distance: "400px",
+    delay: 600
+});
+sa.reveal(".animate-bottom", {
+    origin: "bottom",
+    duration: 1000,
+    distance: "400px",
+    delay: 600
+});
+
+
