d8fe363da05821c131af0caecec2db1b2b15ad34
Amer, Qsem <amerm4011@gmail.com>
2022-04-08 07:38:39 +0800

    edite project section and activate email form

--- script.js
+++ script.js
@@ -2,46 +2,44 @@
 let menuToggler = document.querySelector(".menu-toggle");
 let body = document.querySelector("body");
 menuToggler.onclick = function () {
-    body.classList.toggle("open");
+  body.classList.toggle("open");
 };
 
 // Change navbar color on scroll
 let myNav = document.getElementById("myNav");
 window.onscroll = function () {
-    if (window.scrollY > 680) {
-        myNav.style.backgroundColor = "#252525";
-        myNav.style.boxShadow = "0 0 10px #252525";
-    } else {
-        myNav.style.backgroundColor = "transparent";
-        myNav.style.boxShadow = "none";
-    };
+  if (window.scrollY > 680) {
+    myNav.style.backgroundColor = "#252525";
+    myNav.style.boxShadow = "0 0 10px #252525";
+  } else {
+    myNav.style.backgroundColor = "transparent";
+    myNav.style.boxShadow = "none";
+  }
 };
 
 // Scroll Animation
 window.sa = ScrollReveal();
 sa.reveal(".animate-left", {
-    origin: "left",
-    duration: 1000,
-    distance: "400px",
-    delay: 300
+  origin: "left",
+  duration: 1000,
+  distance: "400px",
+  delay: 300,
 });
 sa.reveal(".animate-right", {
-    origin: "right",
-    duration: 1000,
-    distance: "400px",
-    delay: 600
+  origin: "right",
+  duration: 1000,
+  distance: "400px",
+  delay: 600,
 });
 sa.reveal(".animate-top", {
-    origin: "top",
-    duration: 1000,
-    distance: "400px",
-    delay: 600
+  origin: "top",
+  duration: 1000,
+  distance: "400px",
+  delay: 600,
 });
 sa.reveal(".animate-bottom", {
-    origin: "bottom",
-    duration: 1000,
-    distance: "400px",
-    delay: 600
+  origin: "bottom",
+  duration: 1000,
+  distance: "400px",
+  delay: 600,
 });
-
-
